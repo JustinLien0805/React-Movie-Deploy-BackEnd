b7403c210897b9ba94fa84053be167fadbcdc9e7
JustinLien0805 <6998dragoan@gmail.com>
2022-06-01 12:13:49 +0800

    add spinner

--- index.js
+++ index.js
@@ -7,30 +7,6 @@ import { validateToken } from "./middleware/AuthMiddleware";
 const prisma = new PrismaClient();
 
 const app = express();
-// // Add headers before the routes are defined
-// app.use(function (req, res, next) {
-//   // Website you wish to allow to connect
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-
-//   // Request methods you wish to allow
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-//   );
-
-//   // Request headers you wish to allow
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "X-Requested-With,content-type"
-//   );
-
-//   // Set to true if you need the website to include cookies in the requests sent
-//   // to the API (e.g. in case you use sessions)
-//   res.setHeader("Access-Control-Allow-Credentials", true);
-
-//   // Pass to next layer of middleware
-//   next();
-// });
 app.use(
   cors({
     origin: "https://profound-seahorse-6f3f2b.netlify.app",
